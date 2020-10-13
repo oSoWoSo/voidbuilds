fc5d9991b0d108248c078cfad2feaff9f2847431
John Lupsa <starrbeartony14@zoho.com>
2020-10-13 01:01:08 +0000

    Added addtional menus

--- gengpg.sh
+++ gengpg.sh
@@ -27,4 +27,59 @@ function gen_gpg(){
   done
   }
 
-  gen_gpg
+
+function main_menu(){
+  
+
+  PS3="Enter a number: "
+
+  select option in "Generate GPG Key" "Export GPG Key" "Import GPG Key" "Sign File" "Quit"
+  do
+      if [ "${REPLY}" = "1" ];then
+        gen_gpg
+      
+      elif [ "${REPLY}" = "2" ];then
+        echo "What key would you like to export?"
+        
+        gpg --list-keys
+        echo "enter the email of the gpg key"
+        read EMAIL_ADDRESS
+
+        echo "What do you want to name your file?: "
+        read GPG_FILE_NAME
+        
+        gpg --output "${GPG_FILE_NAME}.asc" --export ${EMAIL_ADDRESS}
+      
+        
+      elif [ "${REPLY}" = "3" ]; then
+        gpg --list-keys
+        echo -e -n "put the full path and name of the gpg key you want to import"
+        read IMPORT_GPG
+
+        gpg --import "${IMPORT_GPG}"
+        gpg --list-keys
+        return
+
+      elif [ "${REPLY}" = "4" ];then
+        if [ ! -f sign-file.sh ];then
+          echo "Cannot find the sign file script"
+        else
+
+          echo -n "Enter full path of file you want to sign: "
+          read FILE_TO_SIGN
+
+          ./sign-file.sh -f "${FILE_TO_SIGN}"
+        fi
+          
+
+      elif [ "${REPLY}" = "5" ];then
+        echo "GoodBye!"
+        exit 0
+      fi
+  done
+
+
+
+}
+
+main_menu
